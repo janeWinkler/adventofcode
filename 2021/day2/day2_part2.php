f4f1c251f5945dc297d2f7112b55118dfcf9b6d6
joshtinkler-josephi <jtinklerjosephi@gmail.com>
2022-10-20 22:14:14 -0400

    Day 2 part 2

---
+++ 2021/day2/day2_part2.php
@@ -0,0 +1,27 @@
+<?php
+    include 'input.php';
+    $arr= preg_split('/([\s]+| \n)/',$input);
+
+    $aim = 0;
+    $depth = 0;
+    $horizontal = 0;
+
+    for($x =0; $x< count($arr); $x++){
+        switch ($arr[$x]) {
+            case 'forward':
+                $horizontal += $arr[$x + 1];
+                $depth += $arr[$x + 1] * $aim;
+                break;
+            case 'up':
+                $aim -= $arr[$x + 1];
+                break;
+            case 'down':
+                $aim += $arr[$x + 1];
+                break;
+
+            default:
+                break;
+        }
+    };
+
+    echo $horizontal * $depth;
